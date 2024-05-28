4c71479fd0df065b8e1dc1bd6dcdb0d33bf64d9c
VinayKumar018 <kumarvinay15381@gmail.com>
2024-05-28 18:10:19 +0530

    Updated Navbar with white background and black text

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,97 @@
 import React from 'react';
-import { Container, Card } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 import './Home.css';
 
 function Home() {
   return (
-    <Container>
-      <Card className="welcome-card">
-        <Card.Body>
-          <Card.Title>Welcome to Devnati Projects</Card.Title>
-          <Card.Text>
-            Explore our projects and learn more about what we do.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Container>
+    <div>
+    <Carousel>
+    <Carousel.Item>
+      <img className="d-block w-100" src="/anime-girls-anime-nekomimi-red-wallpaper-preview.jpg" alt="First slide" />
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img className="d-block w-100" src="anime-manga-anime-girls-simple-background-wallpaper-preview.jpg" alt="Second slide" />
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img className="d-block w-100" src="wonder-egg-priority-ai-ooto-artwork-anime-girls-anime-hd-wallpaper-preview.jpg" alt="Third slide" />
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+  <div className="mt-5">  <div className="container">
+      <div className="row">
+        {/* First Row */}
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="https://www.powercitynetwork.com/assets/images/voip/computer-services-and-support.png" className="card-img-top" alt="Image 1" />
+            <div className="card-body">
+              <h5 className="card-title">Card 1</h5>
+              <p className="card-text">Some text to explain about card 1.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="https://www.powercitynetwork.com/assets/images/voip/computer-services-and-support.png" className="card-img-top" alt="Image 2" />
+            <div className="card-body">
+              <h5 className="card-title">Card 2</h5>
+              <p className="card-text">Some text to explain about card 2.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="https://www.powercitynetwork.com/assets/images/voip/computer-services-and-support.png" className="card-img-top" alt="Image 3" />
+            <div className="card-body">
+              <h5 className="card-title">Card 3</h5>
+              <p className="card-text">Some text to explain about card 3.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        {/* Second Row (Repeat) */}
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="https://www.powercitynetwork.com/assets/images/voip/computer-services-and-support.png" className="card-img-top" alt="Image 4" />
+            <div className="card-body">
+              <h5 className="card-title">Card 4</h5>
+              <p className="card-text">Some text to explain about card 4.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="https://www.powercitynetwork.com/assets/images/voip/computer-services-and-support.png" className="card-img-top" alt="Image 5" />
+            <div className="card-body">
+              <h5 className="card-title">Card 5</h5>
+              <p className="card-text">Some text to explain about card 5.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 mb-4">
+          <div className="card">
+            <img src="https://www.powercitynetwork.com/assets/images/voip/computer-services-and-support.png" className="card-img-top" alt="Image 6" />
+            <div className="card-body">
+              <h5 className="card-title">Card 6</h5>
+              <p className="card-text">Some text to explain about card 6.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div></div>
+  </div>
+
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,20 +3,39 @@ import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 function NavigationBar() {
+  
+  
+
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand as={NavLink} exact to="/">DEVNATI</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link as={NavLink} exact to="/">Home</Nav.Link>
-          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
-          <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
-          <Nav.Link as={NavLink} to="/gallery">Gallery</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <Navbar bg="light" variant="light" expand="lg">
+      
+      
+    <Navbar.Brand as={NavLink} exact to="/">
+    <span className='ml-2'>
+      <img
+        src="https://www.svgrepo.com/show/434497/android.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="Android Logo"
+      />
+      </span>
+     
+      <span className='ml-2'>Logo</span>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link as={NavLink} exact to="/" className="text-dark">Home</Nav.Link>
+        <Nav.Link as={NavLink} to="/about" className="text-dark">About</Nav.Link>
+        <Nav.Link as={NavLink} to="/contact" className="text-dark">Contact</Nav.Link>
+        <Nav.Link as={NavLink} to="/gallery" className="text-dark">Gallery</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
   );
-}
+};
+
 
 export default NavigationBar;
